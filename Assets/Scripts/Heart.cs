8c2748a3761e12d2bab768585c2dc833c87233e1
Max Bize <mbize2005@gmail.com>
2020-04-18 17:12:17 -0700

    More sprites

---
+++ Assets/Scripts/Heart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    // Set in editor
+    public Transform player;
+    public List<Transform> linkStarts;
+    public List<Transform> linkEnds;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
